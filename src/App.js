ffcc9d41d431a7493694d8335ebb87bb91818503
Harish1651999 <hk1651999@gmail.com>
2023-02-06 09:15:21 +0530

    add sending a GET request.

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import MovieList from "./components/MovieList";
 
-const dummyMovies = [
-  {
-    id: 1,
-    title: "Some Dummy Movie 1",
-    openingText: "This is the first opening text of the movie.",
-    releaseDate: "2023-05-16",
-  },
-  {
-    id: 2,
-    title: "Some Dummy Movie 2",
-    openingText: "This is the second opening text of the movie.",
-    releaseDate: "2023-05-19",
-  },
-];
-
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  function fetchMoviesHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformedMovies);
+      });
+  }
+
   return (
     <>
       <section>
-        <button>Fetch Movies</button>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MovieList movies={dummyMovies} />
+        <MovieList movies={movies} />
       </section>
     </>
   );
